63465affc4b360b401fa155ae74f61b941c229f0
Matt Tierney <tierney@cs.nyu.edu>
2013-06-18 15:05:01 -0400

    wip

--- wiki/wiki_main.c
+++ wiki/wiki_main.c
@@ -6,62 +6,62 @@
 
 int yyparse(SExpression **expression, yyscan_t scanner);
 
-SExpression *getAST(const char *expr)
-{
-    SExpression *expression;
-    yyscan_t scanner;
-    YY_BUFFER_STATE state;
+SExpression *getAST(const char *expr) {
+  SExpression *expression;
+  yyscan_t scanner;
+  YY_BUFFER_STATE state;
 
-    if (yylex_init(&scanner))
-    {
-        // couldn't initialize
-        return NULL;
-    }
+  if (yylex_init(&scanner))
+  {
+    // couldn't initialize
+    return NULL;
+  }
 
-    state = yy_scan_string(expr, scanner);
+  state = yy_scan_string(expr, scanner);
 
-    if (yyparse(&expression, scanner))
-    {
-        // error parsing
-        return NULL;
-    }
+  if (yyparse(&expression, scanner))
+  {
+    // error parsing
+    return NULL;
+  }
 
-    yy_delete_buffer(state, scanner);
+  yy_delete_buffer(state, scanner);
 
-    yylex_destroy(scanner);
+  yylex_destroy(scanner);
 
-    return expression;
+  return expression;
 }
 
-int evaluate(SExpression *e)
-{
-    switch (e->type)
-    {
-        case eVALUE:
-            return e->value;
-        case eMULTIPLY:
-            return evaluate(e->left) * evaluate(e->right);
-        case ePLUS:
-            return evaluate(e->left) + evaluate(e->right);
-        default:
-            // shouldn't be here
-            return 0;
-    }
+int evaluate(SExpression *e) {
+  switch (e->type) {
+    case eVALUE:
+      return e->value;
+    case eMULTIPLY:
+      return evaluate(e->left) * evaluate(e->right);
+    case ePLUS:
+      return evaluate(e->left) + evaluate(e->right);
+    case eAND:
+      return evaluate(e->left) && evaluate(e->right);
+    case eOR:
+      return evaluate(e->left) || evaluate(e->right);
+    default:
+      // shouldn't be here
+      return 0;
+  }
 }
 
-int main(void)
-{
-    SExpression *e = NULL;
-    char test[]=" 4 + 2*10 + 3*( 5 + 1 )";
-    int result = 0;
+int main(int argc, char **argv) {
+  SExpression *e = NULL;
+  char test[]=" 4 + 2*10 + 3*( 5 + 1 )";
+  int result = 0;
 
-    e = getAST(test);
+  e = getAST(test);
 
-    result = evaluate(e);
+  result = evaluate(e);
 
-    printf("Result of '%s' is %d\n", test, result);
+  printf("Result of '%s' is %d\n", test, result);
 
-    deleteExpression(e);
+  deleteExpression(e);
 
-    return 0;
+  return 0;
 }
